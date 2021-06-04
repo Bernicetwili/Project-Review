9cb9a7ae403be7a73f3f7012b7c95a0f5b048557
Bernice Twili <bernicetwili0@gmail.com>
2021-06-04 20:27:30 +0300

    configure main url

---
+++ apps/main/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path, re_path
+
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('project/<int:project_id>/', views.single_project, name='single_project'),
+    path('search/', views.search_projects, name='search_projects'),
+    path('project/new/', views.create_project, name='create_project'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('auth/login/', views.loginUser, name='login'),
+    path('auth/signup/', views.signup, name='signup'),
+    path('auth/logout/', views.log_out, name='logout'),
+    path('activation/<uid>/<token>/', views.activate_account, name='activation'),
+]
