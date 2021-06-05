4f625d644b3461a5de1067b61d8afe7bcc8448c4
Bernice Twili <bernicetwili0@gmail.com>
2021-06-05 23:30:25 +0300

    resolve gitignore

--- awards/settings.py
+++ awards/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#ENV_FILE = '.env'
+ENV_FILE = '.env'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
